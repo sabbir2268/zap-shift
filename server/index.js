a904ee0a73df4b70c72b65c3bec300966cbfd263
MD Sabbir Rahman <mdsabbirrahman2268@gmail.com>
2026-09-13 21:42:43 +0600

    mongodb connected successfully

--- server/index.js
+++ server/index.js
@@ -1,11 +1,40 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require('dotenv');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
+dotenv.config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+console.log("PORT:", process.env.PORT);
+
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({
+      ping: 1,
+    });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+  }
+}
+
+run();
 
 app.get("/", (req, res) => {
   res.send("Server is running ");
